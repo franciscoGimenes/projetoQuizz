09d88e4985425c4fc39c1508f99193a76e9a99f9
Gimenes <f.gimenes1316@gmail.com>
2023-08-20 22:02:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,322 @@
+let questoes = [
+    questao = {
+        tituloQuestao: "Qual é a capital do Brasil?",
+        opcoes: ["Belo Horizonte"," Brasília"," Rio de Janeiro","São Paulo"],
+        respostaCorreta: 1,
+        imagemQuestao:"https://www.gov.br/planalto/pt-br/conheca-a-presidencia/acervo/simbolos-nacionais/bandeira/bandeiragrande.jpg",
+        dificuldade: "easy"
+    },
+    questao = {
+        tituloQuestao: "Quem escreveu 'Dom Quixote'?",
+        opcoes: ["Franz Kafka"," Miguel de Cervantes","Machado de Assis","William Shakespeare"],
+        respostaCorreta: 1,
+        imagemQuestao:"https://santhatela.com.br/wp-content/uploads/2021/09/wilhelm-marstrand-dom-quixote-e-sancho-panca-d.jpg",
+        dificuldade: "easy"
+    },
+    questao = {
+        tituloQuestao: "Qual é o maior planeta do sistema solar?",
+        opcoes: ["Terra","Marte","Júpiter","Vênus"],
+        respostaCorreta: 2,
+        imagemQuestao:"https://s1.static.brasilescola.uol.com.br/be/2022/09/2-sistema-solar.jpg",
+        dificuldade: "easy"
+    },
+    questao = {
+        tituloQuestao: "Qual é o autor de 'Romeu e Julieta'?",
+        opcoes: ["William Shakespeare","William Faulkner","Jane Austen"," Charles Dickens"],
+        respostaCorreta: 0,
+        imagemQuestao:"https://www.britishcouncil.org.br/sites/default/files/romeo-juliet-movie.jpg",
+        dificuldade: "easy"
+    },
+    questao = {
+        tituloQuestao: "Qual é o maior oceano do mundo?",
+        opcoes: ["Atlântico","Pacífico","Índico","Ártico"],
+        respostaCorreta: 1,
+        imagemQuestao:"https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/06/localidade-oceanos.jpg",
+        dificuldade: "easy"
+    },
+    questao = {
+        tituloQuestao: "Quem foi o primeiro presidente do Brasil após a redemocratização em 1985?",
+        opcoes: [" Itamar Franco"," Tancredo Neves"," Fernando Collor de Mello","Juscelino Kubitschek"],
+        respostaCorreta: 1,
+        imagemQuestao:"https://blogtuliolemos.com.br/wp-content/uploads/2021/04/tancredonevescapa5_widexl.jpeg",
+        dificuldade: "medium"
+    },
+    questao = {
+        tituloQuestao: "Qual é o autor de 'Grande Sertão: Veredas'?",
+        opcoes: [" José Saramago","Jorge Amado","Guimarães Rosa","Clarice Lispector"],
+        respostaCorreta: 2,
+        imagemQuestao:"https://cdn.pensador.com/img/imagens/gu/im/guimaraes_rosa_2.jpg",
+        dificuldade: "medium"
+    },
+    questao = {
+        tituloQuestao: "Quem foi o primeiro astronauta a caminhar na Lua?",
+        opcoes: ["Alan Shepard","John Glenn","Neil Armstrong","Yuri Gagarin"],
+        respostaCorreta: 2,
+        imagemQuestao:"https://static.todamateria.com.br/upload/ne/il/neilarmstrong-cke.jpg",
+        dificuldade: "medium"
+    },
+    questao = {
+        tituloQuestao: "Qual é o processo de transferência de calor que ocorre apenas em meios materiais?",
+        opcoes: ["Fusão","Radiação","Convecção","Condução"],
+        respostaCorreta: 3,
+        imagemQuestao:"https://static.preparaenem.com/2020/11/1-radiacao-conveccao-conducao.jpg",
+        dificuldade: "medium"
+    },
+    questao = {
+        tituloQuestao: "Quem foi o fundador da psicanálise?",
+        opcoes: ["Ivan Pavlov","B.F. Skinner","Sigmund Freud","Carl Jung"],
+        respostaCorreta: 2,
+        imagemQuestao:"https://s1.static.brasilescola.uol.com.br/be/conteudo/images/sigmund-freud.jpg",
+        dificuldade: "medium"
+    },
+    questao = {
+        tituloQuestao: "Qual é o nome do tratado que encerrou a Primeira Guerra Mundial?",
+        opcoes: ["Tratado de Versalhes","Tratado de Tordesilhas","Tratado de Yalta","Tratado de Paris"],
+        respostaCorreta: 0,
+        imagemQuestao:"https://static.historiadomundo.com.br/2019/06/assinatura-do-tratado-de-versalhes.jpg",
+        dificuldade: "hard"
+    },
+    questao = {
+        tituloQuestao: "Quem foi o pintor italiano conhecido por suas obras como 'Mona Lisa' e 'A Última Ceia'?",
+        opcoes: ["Vincent van Gogh","Pablo Picasso","Leonardo da Vinci","Michelangelo Buonarroti"],
+        respostaCorreta: 2,
+        imagemQuestao:"https://cinemacao.com/wp-content/uploads/2014/06/leonardo_destaque.jpg",
+        dificuldade: "hard"
+    },
+    questao = {
+        tituloQuestao: "Qual é a fórmula química do dióxido de carbono?",
+        opcoes: ["CO","CO2","O2","H20"],
+        respostaCorreta: 1,
+        imagemQuestao:"https://s2.glbimg.com/YK5sDcwVRxMpv9Uo1kke1xiAzRw=/e.glbimg.com/og/ed/f/original/2022/03/09/ella-ivanescu-jbfhnrpq_dw-unsplash.jpg",
+        dificuldade: "hard"
+    },
+    questao = {
+        tituloQuestao: "Qual é o autor da obra '1984', que descreve um mundo totalitário distópico?",
+        opcoes: ["George Orwell","Aldous Huxley","Ray Bradbury","Kurt Vonnegut"],
+        respostaCorreta: 0,
+        imagemQuestao:"https://ufmg.br/thumbor/mzaLJwM-XeRRzDDp3BUb0ps4nUw=/0x167:776x683/712x474/https://ufmg.br/storage/c/d/1/f/cd1feb118e5c245c847576f35afefe56_1572304504401_1601870368.jpg",
+        dificuldade: "hard"
+    },
+    questao = {
+        tituloQuestao: "Qual filósofo grego é conhecido por suas contribuições à ética e à filosofia política, e escreveu a obra 'Ética a Nicômaco'?",
+        opcoes: ["Sócrates","Platão","Aristóteles","Epicuro"],
+        respostaCorreta: 2,
+        imagemQuestao:"https://www.infoescola.com/wp-content/uploads/2008/06/platao.jpg",
+        dificuldade: "hard"
+    },
+]
+const btn0 = document.querySelector("#btn-0"),
+    btn1 = document.querySelector("#btn-1"),
+    btn2 = document.querySelector("#btn-2"),
+    btn3 = document.querySelector("#btn-3"),
+    btnIniciar = document.querySelector("#iniciar")
+    comecoDiv = document.querySelector('#main'),
+    questoesDiv = document.querySelector('#questoes'),
+    fimDiv = document.querySelector('#fim'),
+    indicador = document.querySelector('#atualQuestao'),
+    Titulo = document.querySelector('#tituloDaQuestao'),
+    imagem = document.querySelector('#img'),
+    reiniciar = document.querySelector('#voltar'),
+    terminoTitulo = document.querySelector('#terminoTitulo'),
+    pontosFinais = document.querySelector('#score'),
+    feedback = document.querySelector("#feedback")
+    
+
+reiniciar.addEventListener('click', () => {
+    comecoDiv.classList.remove('hide')
+    questoesDiv.classList.add('hide')
+    fimDiv.classList.add('hide')
+    score = 0
+    questaoAtual = 0
+})
+btnIniciar.addEventListener('click', () => {
+    vidas = document.querySelector('#vidas').value
+    if (vidas >= 1 && vidas <= 10){
+        comecoDiv.classList.add('hide')
+        questoesDiv.classList.remove('hide')
+        fimDiv.classList.add('hide')
+        carregar() 
+    }
+    else{
+        btnIniciar.style.backgroundColor = 'red'
+        btnIniciar.style.color = "white"
+        btnIniciar.textContent = "INSIRA UM VALOR ENTRE 1 E 10"
+    }
+
+})
+
+
+let questaoAtual = 0,
+  score = 0
+function carregar(){
+    if(questaoAtual < questoes.length){
+        btn0.classList.remove("correct",'wrong')
+        btn1.classList.remove('correct','wrong')
+        btn2.classList.remove('correct','wrong')
+        btn3.classList.remove('correct','wrong')
+        indicador.textContent = questaoAtual+1
+        Titulo.textContent = questoes[questaoAtual].tituloQuestao
+        btn0.textContent = questoes[questaoAtual].opcoes[0]
+        btn1.textContent = questoes[questaoAtual].opcoes[1]
+        btn2.textContent = questoes[questaoAtual].opcoes[2]
+        btn3.textContent = questoes[questaoAtual].opcoes[3]
+        imagem.src = questoes[questaoAtual].imagemQuestao
+    }
+    else{
+        alert(`Você terminou o Quiz!!`)
+        questoesDiv.classList.add('hide')
+        fimDiv.classList.remove('hide')
+        carregarTermino()
+    }
+
+}
+function carregarTermino(){
+    questoesDiv.classList.add('hide')
+    fimDiv.classList.remove('hide')
+    if (vidas > 0){
+        terminoTitulo.textContent = "parabéns, você completou o quizz!"
+        pontosFinais.textContent = `Você marcou ${score} pontos`
+    }
+    else {
+        terminoTitulo.textContent = "Você perdeu"
+        pontosFinais.textContent = `Você marcou ${score} pontos`
+    }
+}
+
+
+
+
+
+btn0.addEventListener('click', () => {
+    resposta = 0
+    respostaCerta = questoes[questaoAtual].respostaCorreta
+    diff = questoes[questaoAtual].dificuldade
+    console.log(respostaCerta)
+    if (resposta == respostaCerta){
+        if (diff == "hard"){
+            score += 30
+            ++questaoAtual
+            carregar()
+        }else if(diff == "medium"){
+            score += 20
+            ++questaoAtual
+            carregar()
+        }else{
+            score += 10
+            ++questaoAtual
+            carregar()
+        }
+    }
+    else{
+        btn0.classList.add("wrong")
+        --vidas
+        if(vidas > 0){
+              alert(`Resposta errada :(, mas voce ainda tem ${vidas} vida(s)!`)
+        }
+        else{
+            alert(`Suas vidas acabaram`)
+            carregarTermino()
+        }
+
+    }
+})
+btn1.addEventListener('click', () => {
+    resposta = 1
+    respostaCerta = questoes[questaoAtual].respostaCorreta
+    console.log(respostaCerta)
+    diff = questoes[questaoAtual].dificuldade
+    if (resposta == respostaCerta){
+        if (diff == "hard"){
+            score += 30
+            ++questaoAtual
+            carregar()
+        }else if(diff == "medium"){
+            score += 20
+            ++questaoAtual
+            carregar()
+        }else{
+            score += 10
+            ++questaoAtual
+            carregar()
+        }
+    }
+    else{
+        btn1.classList.add("wrong")
+        --vidas
+        if(vidas > 0){
+              alert(`Resposta errada :(, mas voce ainda tem ${vidas} vida(s)!`)
+        }
+        else{
+            alert(`Suas vidas acabaram`)
+            carregarTermino()
+        }
+
+    }
+})
+btn2.addEventListener('click', () => {
+    resposta = 2
+    respostaCerta = questoes[questaoAtual].respostaCorreta
+    console.log(respostaCerta)
+    diff = questoes[questaoAtual].dificuldade
+    if (resposta == respostaCerta){
+        if (diff == "hard"){
+            score += 30
+            ++questaoAtual
+            carregar()
+        }else if(diff == "medium"){
+            score += 20
+            ++questaoAtual
+            carregar()
+        }else{
+            score += 10
+            ++questaoAtual
+            carregar()
+        }
+    }
+    else{
+        btn2.classList.add("wrong")
+        --vidas
+        if(vidas > 0){
+              alert(`Resposta errada :(, mas voce ainda tem ${vidas} vida(s)!`)
+        }
+        else{
+            alert(`Suas vidas acabaram`)
+            carregarTermino()
+        }
+
+    }
+})
+btn3.addEventListener('click', () => {
+    resposta = 3
+    respostaCerta = questoes[questaoAtual].respostaCorreta
+    console.log(respostaCerta)
+    diff = questoes[questaoAtual].dificuldade
+    if (resposta == respostaCerta){
+        if (diff == "hard"){
+            score += 30
+            ++questaoAtual
+            carregar()
+        }else if(diff == "medium"){
+            score += 20
+            ++questaoAtual
+            carregar()
+        }else{
+            score += 10
+            ++questaoAtual
+            carregar()
+        }
+    }
+    else{
+        btn3.classList.add("wrong")
+        --vidas
+        if(vidas > 0){
+              alert(`Resposta errada :(, mas voce ainda tem ${vidas} vida(s)!`)
+        }
+        else{
+            alert(`Suas vidas acabaram`)
+            carregarTermino()
+        }
+
+    }
+})
+
